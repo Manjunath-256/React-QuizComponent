23e26565b9cc0363eea1d41521c2023594325605
Manjunath-256 <manjunathmarpalli256@gmail.com>
2021-03-03 19:26:52 +0530

    Module3

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,14 +1,21 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
-let quizData=require('./quiz_data.json')
-class Quiz extends Component{
-constructor(props){
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+
+ constructor(props){
 super(props);
-this.state= {quiz_position:1};
+this.state = {
+quiz_position: 1
+};
 }
-render(){
-return(
-<QuizQuestion quiz_question={quizData.quiz_questions[0]}/>
+
+ render() {
+return (
+
+ <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]} />
 );
 }
 }
