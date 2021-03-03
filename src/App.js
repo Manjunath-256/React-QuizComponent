c9cadc15a4a5fa9a2de6b8cd003b58fced48e5a6
Manjunath- <manjunathmarpalli256@gmail.com>
2021-03-03 17:36:14 +0530

    Module2 Solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
-
+import Quiz from './Quiz.js'
 class App extends Component {
-  render() {
-    return (
-      <div />
-    )
-  }
+render(){
+return (
+<Quiz />
+)
 }
+}
+
+
+
+
 
 export default App
